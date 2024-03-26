ec78df30f49b1a2f45e3217c027ab0ec66725b85
Шлыков Дмитрий <ateaohe@yandex.ru>
2024-03-27 00:06:58 +0300

    user router

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -10,13 +10,21 @@ router.include_router(
     prefix='/auth/jwt',
     tags=['auth'],
 )
+
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix='/auth',
     tags=['auth'],
 )
+
+users_router = fastapi_users.get_users_router(UserRead, UserUpdate)
+
+users_router.routes = [
+    rout for rout in users_router.routes if rout.name != 'users:delete_user'
+]
+
 router.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
+    users_router,
     prefix='/users',
     tags=['users'],
 )
