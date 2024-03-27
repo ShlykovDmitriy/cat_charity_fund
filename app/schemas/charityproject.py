0944775701fda52df6d9b5a834a793058d6c04ca
Шлыков Дмитрий <ateaohe@yandex.ru>
2024-03-28 01:09:34 +0300

    фÐadd schemas

---
+++ app/schemas/charityproject.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, PositiveInt, Extra, Field
+
+
+class ProjectCreate(BaseModel):
+    name: Optional[str] = Field(..., min_length=1, max_length=100)
+    description: Optional[str] = Field(..., min_length=1)
+    full_amount: Optional[PositiveInt]
+
+    class Config:
+        extra = Extra.forbid
+
+
+class ProjectUpdate(ProjectCreate):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    description: Optional[str] = Field(None, min_length=1)
+
+
+class AllProjectGet(ProjectCreate):
+    id: int
+    invested_amount: int
+    fully_invested: bool
+    create_date: datetime
+    close_date: Optional[datetime]
+
+    class Config:
+        orm_mode = True
